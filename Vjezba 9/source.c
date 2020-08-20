2327a3d29fe90c7e774881afc260a81a17a7c0fc
peko99 <marin.pejcin@gmail.com>
2020-08-20 13:03:30 +0200

    9 zadatak

--- Vjezba 9/source.c
+++ Vjezba 9/source.c
@@ -4,8 +4,136 @@
 #include<malloc.h>
 #include<string.h>
 
+typedef struct _stablo {
+    int element;
+
+    struct _stablo* left;
+    struct _stablo* right;
+}Stablo;
+
+int printMenu();
+Stablo* Menu(Stablo*);
+Stablo* insertNew(Stablo*, int);
+int printTree(Stablo*);
+Stablo* searchTree(Stablo*, int);
+Stablo* deleteElement(Stablo*, int);
+
 int main()
 {
+    Stablo* root = NULL;
+
+    printMenu();
+
+    do {
+        root = Menu(root);
+    } while (root);
 
     return 0;
+}
+
+int printMenu()
+{
+    printf("****************************************\n");
+    printf("1) Unesite novi element\n");
+    printf("2) Ispisite elemente iz stabla\n");
+    printf("3) Pronadite element u stablu\n");
+    printf("4) Izbrisite element iz stabla\n");
+    printf("5) Ponovni ispis izbornika\n");
+    printf("0) Izlaz iz programa\n");
+    printf("****************************************\n");
+
+    return 0;
+}
+
+Stablo* Menu(Stablo* root)
+{
+    int izbor, unesi, trazi, brisi;
+    Stablo* element = (Stablo*)malloc(sizeof(Stablo));
+    element->left = NULL;
+    element->right = NULL;
+
+    printf("\n--> ");
+    scanf(" %d", &izbor);
+
+    switch (izbor) {
+    case 0:
+        return NULL;
+    case 1:
+        printf("\nKoji element zelite unijeti u stablo? ");
+        scanf(" %d", &unesi);
+        root = insertNew(root, unesi);
+        break;
+    case 2:
+        printf("\n");
+        printTree(root);
+        break;
+    case 3:
+        printf("\nKoji element zelite pronaci? ");
+        scanf(" %d", &trazi);
+        element = searchTree(root, trazi);
+        if(element != NULL)
+            printf("\nElement koji ste trazili | %d |\n", element->element);
+        break;
+    case 4:
+        printf("\nKoji element zelite izbrisati? ");
+        scanf(" %d", &brisi);
+        root = deleteElement(root, brisi);
+        break;
+    case 5:
+        printMenu();
+        break;
+    default:
+        printf("\nKrivi unos! Pokusajte ponovo\n");
+    }
+
+    return root;
+}
+
+Stablo* insertNew(Stablo* root, int unesi)
+{
+    if (root == NULL) {
+        root = (Stablo*)malloc(sizeof(Stablo));
+        root->left = NULL;
+        root->right = NULL;
+        root->element = unesi;
+    }
+
+    else if (unesi > root->element) {
+        root->right = insertNew(root->right, unesi);
+    }
+    else if (unesi < root->element) {
+        root->left = insertNew(root->left, unesi);
+    }
+
+    return root;
+}
+
+int printTree(Stablo* root)
+{
+    if (root == NULL) {
+        return 0;
+    }
+
+    printTree(root->left);
+    printf(" %d", root->element);
+    printTree(root->right);
+
+    return 0;
+}
+
+Stablo* searchTree(Stablo* root, int broj)
+{
+    if (root == NULL) {
+        printf("\nElement nije pronaden u stablu\n");
+        return NULL;
+    }
+
+    else if (broj > root->element)
+        return searchTree(root->right, broj);
+
+    else if (broj < root->element)
+        return searchTree(root->left, broj);
+
+    else if (broj == root->element)
+        return root;
 }
