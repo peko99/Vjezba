8a5dedb9cb2935c3b306fb10a3c196193e5a1037
peko 99 <marin.pejcin@gmail.com>
2020-08-21 20:07:38 +0200

    ispitni primjer 1

--- Ispitni primjer 1/source.c
+++ Ispitni primjer 1/source.c
@@ -15,7 +15,9 @@ typedef struct _stablo{
     struct _stablo* right;
 }Stablo;
 
-
+Stablo* procitajDat(Stablo*, FILE*);
+Stablo* insertSorted(Stablo*, char*);
+int print(Stablo*);
 
 int main()
 {
@@ -38,6 +40,58 @@ int main()
     
     root = procitajDat(root, fp);
     print(root);
+
+    return 0;
+}
+
+Stablo* procitajDat(Stablo* root, FILE* fp)
+{
+    char* line = (char*)malloc(sizeof(char)*LINE_LEN);
+    int n;
+    char* word = (char*)malloc(sizeof(char)*WORD_LEN);
+
+    fgets(line, LINE_LEN, fp);
+
+    while(*line != 0){
+        sscanf(line, "%s %n", word, &n);
+        line += n;
+        root = insertSorted(root, word);
+    }
+
+    return root;
+}
+
+Stablo* insertSorted(Stablo* root, char* word)
+{
+    if(root == NULL){
+        root = (Stablo*)malloc(sizeof(Stablo));
+        root->rijec = (char*)malloc(sizeof(char)*WORD_LEN);
+        strcpy(root->rijec, word);
+        root->duljina = strlen(root->rijec);
+        root->left = NULL;
+        root->right = NULL;
+        
+        return root;
+    }
+
+    else if(strcmp(root->rijec, word) > 0)
+        root->left = insertSorted(root->left, word);
+
+    else if(strcmp(root->rijec, word) < 0)
+        root->right = insertSorted(root->right, word);
+
+    else
+        return root;    
+}
+
+int print(Stablo* root)
+{
+    if(root == NULL)
+        return 0;
+
+    print(root->left);
+    printf("%s\t%d", root->rijec, root->duljina);
+    print(root->right);
 
     return 0;
 }
